a2c4ca54d8ec46473400af007f47d12139126c84
shiv123736 <dharmasya0@gmail.com>
2026-01-02 13:22:00 +0530

    chore: teammate changed message to ENTERPRISE

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 
 export const getHome = (_req: Request, res: Response) => {
     res.json({
-        message: 'Welcome to the Professional API',
+        message: 'Welcome to the ENTERPRISE API',
         status: 'success',
         timestamp: new Date().toISOString()
     });
